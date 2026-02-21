c0773183e1f274de744885b93e9504e238662224
Harsh Wardhan Singh <harshwardhan1621@gmail.com>
2026-02-21 12:00:17 +0530

    Added Health

--- app.py
+++ app.py
@@ -21,6 +21,10 @@ def analyze():
             "error": str(e)
         }), 400
 
+@app.route("/health")
+def health():
+    return "OK", 200
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
